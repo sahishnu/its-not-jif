f6f2a0221443c7f3da6b75db5a34b588d8948dd8
Sash Patel <bhoolku5443@gmail.com>
2018-11-25 23:50:11 -0500

    Updates

--- precache-manifest.1df82a65c9503124734630ec88233ef6.js
+++ precache-manifest.1df82a65c9503124734630ec88233ef6.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/its-not-jif/static/js/runtime~main.1c806ffc.js"
   },
   {
-    "revision": "a16f512c95a5a925e28f",
-    "url": "/its-not-jif/static/js/main.a16f512c.chunk.js"
+    "revision": "a27b55f1a94ef6e3acce",
+    "url": "/its-not-jif/static/js/main.a27b55f1.chunk.js"
   },
   {
     "revision": "8c930d6613ba0e9284b0",
     "url": "/its-not-jif/static/js/1.8c930d66.chunk.js"
   },
   {
-    "revision": "a16f512c95a5a925e28f",
-    "url": "/its-not-jif/static/css/main.23b73726.chunk.css"
+    "revision": "a27b55f1a94ef6e3acce",
+    "url": "/its-not-jif/static/css/main.953d6adf.chunk.css"
   },
   {
-    "revision": "ee18b3fa5c9da80a98ac79c895e2af89",
+    "revision": "6d013035457a872cc06624ae5d7df431",
     "url": "/its-not-jif/index.html"
   }
 ];
