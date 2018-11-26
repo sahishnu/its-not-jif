99a4eba3c99b23556bb415d831a58646ff6d27ac
Sash Patel <bhoolku5443@gmail.com>
2018-11-25 23:49:57 -0500

    fix responsive view

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,14 @@ class App extends Component {
     return (
       <div className='App'>
         {/* Search header always shows */}
-        <Header />
+        <Header classname='header-container' />
         <Switch>
           {/* Only difference between Home & Favorites component is where the Gif Data comes from */}
           <Route path='/search' component={Home} />
           <Route path='/favorites' component={Favorites} />
           <Route path='/' component={Home} />
         </Switch>
+        <Header classname='footer-container' />
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,9 @@ import SearchBox from './SearchBox';
 import { Link, NavLink } from 'react-router-dom';
 class Header extends Component {
   render() {
+    const { classname } = this.props;
     return (
-      <div className='header-container'>
+      <div className={classname}>
         <Link to='/' className='header-title title'>Its Not Jif</Link>
         <SearchBox />
         <NavLink activeClassName='active-link' to='/favorites' className='header-title fav'>Favorites</NavLink>
